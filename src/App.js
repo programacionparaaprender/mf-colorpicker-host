f82cf187672272a6d2f26ed2b4665bae44495e60
programacionparaaprender <yancel209@gmail.com>
2025-11-28 19:00:22 -0500

    agregando react 18

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Host Microfrontend</h1>
+      <p>Proyecto Host funcionando.</p>
+    </div>
+  );
+}
